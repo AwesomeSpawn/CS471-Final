b125109463a88623e178882a6c1483154323ae1f
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-10-30 21:47:53 -0400

    Landing Page & Templates
    Added landing page & templates folder along with .gitignore

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+def landing_page(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page),
 ]
