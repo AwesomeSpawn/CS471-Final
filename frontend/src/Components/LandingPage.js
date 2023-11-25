f4eac557095e3a92e8f8d87c365d95049f5ffc63
RichmondAB <richmondben285@outlook.com>
2023-11-24 19:11:18 -0500

    Added Basic Roles to landing page

--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -4,8 +4,17 @@ import AppButton from "./AppButton";
 import './LandingPage.css'
 import { useState } from "react";
 
+let technician_apps = ['jobs', 'timesheet'];
+let manager_apps = ['jobs_manage', 'timesheet', 'timesheet_manage'];
+let cashier_apps = ['timesheet']
+let employee_apps = ['timesheet']
+
 function LandingPage(props) {
-    const apps = ['jobs', 'timesheet']
+    let apps = [];
+    if (props.role === 'technician') apps = technician_apps;
+    else if (props.role === 'manager') apps = manager_apps;
+    else if (props.role === 'cashier') apps = cashier_apps;
+    else apps = employee_apps;
     console.log(props.role);
     return(
         <div>
