cf5af0ce20b03f63745f1ec36d8ab1ce1cdbc9ac
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-12-05 21:16:26 -0500

    Remove unused code and environment variable loading

--- website/website/settings.py
+++ website/website/settings.py
@@ -12,15 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import environ
 
-# Initialize environment variables
-env = environ.Env()
-# Reading .env file
-environ.Env.read_env()
-
-MY_API_KEY = env('MY_API_KEY')
-print("Loaded API Key:", MY_API_KEY)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
